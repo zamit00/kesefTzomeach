2178efc4a42d67afd14496b2158d9491e0703216
zamit00 <zamit00@gmail.com>
2024-12-10 16:34:32 +0200

    Update loanscript.js

--- loanscript.js
+++ loanscript.js
@@ -1,5 +1,19 @@
 
+		const select=document.getElementById('grace');
+        for (let i = 1; i <= 36; i++) {
+        let option = document.createElement('option');
+        option.value = i;  
+        option.textContent = i;  
+        select.appendChild(option);  
+    }
 
+	function addgrace(x){
+		const gr=document.getElementById('grace');
+		if(x.checked){
+			gr.style.display="block";
+		}
+		else{gr.style.display="none";}
+	}
 
 
 function silukin(){
@@ -8,13 +22,36 @@ function silukin(){
   const loanTerm = parseInt(document.getElementById('loan-term').value);
   const annualInterestRate = parseFloat(document.getElementById('interest-rate').value) / 100;
   const monthlyInterestRate = annualInterestRate / 12;
-  const monthlyPayment = loanAmount * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, loanTerm)) / (Math.pow(1 + monthlyInterestRate, loanTerm) - 1);
+  const gr=document.getElementById('grace');
+  const chekb=document.getElementById('chb');
+  
+  var loanAmountnew;
+  if(chekb.checked){
+		loanAmountnew=Math.pow(1+monthlyInterestRate,parseInt(gr.value) )*loanAmount;
+		 console.log(loanAmountnew);
+		}
+	else{loanAmountnew=loanAmount};
+  
+  const monthlyPayment = loanAmountnew * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, loanTerm)) / (Math.pow(1 + monthlyInterestRate, loanTerm) - 1);
   
   
   const tableBody = document.querySelector('#amortization-schedule tbody');
   tableBody.innerHTML = ''; // ניקוי הטבלה
 
-  let remainingBalance = loanAmount;
+	if(chekb.checked){
+		const row0 = document.createElement('tr');
+		const ribitgrace=loanAmountnew-loanAmount;
+		 row0.innerHTML =`
+      <td>${0}</td>
+      <td></td>
+      <td></td>
+      <td>${ribitgrace.toFixed(2)}</td>
+      <td>${loanAmountnew.toFixed(2)}</td>
+    `
+	tableBody.appendChild(row0);
+	}
+
+  let remainingBalance = loanAmountnew;
 
   for (let i = 1; i <= loanTerm; i++) {
     const interestPayment = remainingBalance * monthlyInterestRate;
@@ -39,19 +76,19 @@ function silukin(){
           let cell2 = table.rows[i].cells[2];
           let cell3 = table.rows[i].cells[3];
           let cell4 = table.rows[i].cells[4];
-          let value = parseInt(cell.textContent); // המרת הטקסט למספר
+          let value = parseFloat(cell.textContent); // המרת הטקסט למספר
           if (!isNaN(value)) {
               cell.textContent = value.toLocaleString(); // המרת המספר לפורמט מקומי
           }
-          value = parseInt(cell2.textContent);
+          value = parseFloat(cell2.textContent);
           if (!isNaN(value)) {
             cell2.textContent = value.toLocaleString(); // המרת המספר לפורמט מקומי
         }
-        value = parseInt(cell3.textContent);
+        value = parseFloat(cell3.textContent);
         if (!isNaN(value)) {
           cell3.textContent = value.toLocaleString(); // המרת המספר לפורמט מקומי
       }
-      value = parseInt(cell4.textContent);
+      value = parseFloat(cell4.textContent);
       if (!isNaN(value)) {
         cell4.textContent = value.toLocaleString(); // המרת המספר לפורמט מקומי
     }
@@ -59,13 +96,19 @@ function silukin(){
  table.style.display="block";
  
 }
+	
 
-
-document.getElementById('calculate-btn').addEventListener('click', function () {
+	
+	
+	document.getElementById('calculate-btn').addEventListener('click', function () {
     const loanAmount = parseFloat(document.getElementById('loan-amount').value);
     const loanTerm = parseInt(document.getElementById('loan-term').value);
     const annualInterestRate = parseFloat(document.getElementById('interest-rate').value) / 100;
-
+	const gr=document.getElementById('grace');
+	const chekb=document.getElementById('chb');
+	
+		
+		
     var opt=document.getElementById('calculate-btn').textContent
     
       if (isNaN(loanAmount) || isNaN(loanTerm) || isNaN(annualInterestRate)) {
@@ -75,8 +118,16 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
       if(opt==='חשב'){
         document.getElementById('calculate-btn').textContent='נקה הכל';
       const monthlyInterestRate = annualInterestRate / 12;
-      const monthlyPayment = loanAmount * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, loanTerm)) / (Math.pow(1 + monthlyInterestRate, loanTerm) - 1);
+	  var loanAmountnew;
+	  if(chekb.checked){
+		 loanAmountnew=Math.pow(1+monthlyInterestRate,parseInt(gr.value) )*loanAmount;
+		 console.log(loanAmountnew);
+		}
+	else{loanAmountnew=loanAmount}
+	  
+      const monthlyPayment = loanAmountnew * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, loanTerm)) / (Math.pow(1 + monthlyInterestRate, loanTerm) - 1);
     
+	
       
       document.getElementById('toz').style.display="block";
       document.getElementById('loachsilukin').style.display="block";
