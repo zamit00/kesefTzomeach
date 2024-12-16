33bff9fb4d87651dcabc16d114728f2dabc6ddd5
zamit00 <zamit00@gmail.com>
2024-12-16 22:03:46 +0200

    Update madadJvs.js

--- madadJvs.js
+++ madadJvs.js
@@ -1,21 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     
     console.log("המסמך נטען!");
-	const fileUrl = 'madadim.xlsx';
+    const fileUrl = 'madadim.xlsx';
 
     fetch(fileUrl)
-                .then(response => response.arrayBuffer())  
-                .then(data => {
-                    const workbook = XLSX.read(data, { type: 'array' });
-                    const sheetName = workbook.SheetNames[0];  
-                    const sheet = workbook.Sheets[sheetName];
+        .then(response => response.arrayBuffer())  
+        .then(data => {
+            const workbook = XLSX.read(data, { type: 'array' });
+            const sheetName = workbook.SheetNames[0];  
+            const sheet = workbook.Sheets[sheetName];
 
-                    const json = XLSX.utils.sheet_to_json(sheet);
-			alert(jason[1][1]);
-                    console.log(json);  // מציג את הנתונים בקונסול
-                })
-                .catch(error => console.error('Error loading Excel file:', error));
-        
+            const json = XLSX.utils.sheet_to_json(sheet);
+            alert(json[1][1]);  // Corrected the typo here: 'jason' -> 'json'
+            console.log(json);  // מציג את הנתונים בקונסול
+        })
+        .catch(error => console.error('Error loading Excel file:', error));
+
 });
 
 
